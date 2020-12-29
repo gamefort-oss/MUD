0eb3ddb435a90bca53be6bcd0e7abd2618fad95b
cabala <40265286+cabala3300@users.noreply.github.com>
2020-12-29 10:01:31 +0800

    删除无用代码

--- Project_MUD/src/Main.ts
+++ Project_MUD/src/Main.ts
@@ -107,49 +107,6 @@ class Main extends eui.UILayer {
         sky.width = stageW;
         sky.height = stageH;
 
-        // let topMask = new egret.Shape();
-        // topMask.graphics.beginFill(0x000000, 0.5);
-        // topMask.graphics.drawRect(0, 0, stageW, 172);
-        // topMask.graphics.endFill();
-        // topMask.y = 33;
-        // this.addChild(topMask);
-
-        // let icon: egret.Bitmap = this.createBitmapByName("egret_icon_png");
-        // this.addChild(icon);
-        // icon.x = 26;
-        // icon.y = 33;
-
-        // let line = new egret.Shape();
-        // line.graphics.lineStyle(2, 0xffffff);
-        // line.graphics.moveTo(0, 0);
-        // line.graphics.lineTo(0, 117);
-        // line.graphics.endFill();
-        // line.x = 172;
-        // line.y = 61;
-        // this.addChild(line);
-
-
-        // let colorLabel = new egret.TextField();
-        // colorLabel.textColor = 0xffffff;
-        // colorLabel.width = stageW - 172;
-        // colorLabel.textAlign = "center";
-        // colorLabel.text = "Hello Egret";
-        // colorLabel.size = 24;
-        // colorLabel.x = 172;
-        // colorLabel.y = 80;
-        // this.addChild(colorLabel);
-
-        // let textfield = new egret.TextField();
-        // this.addChild(textfield);
-        // textfield.alpha = 0;
-        // textfield.width = stageW - 172;
-        // textfield.textAlign = egret.HorizontalAlign.CENTER;
-        // textfield.size = 24;
-        // textfield.textColor = 0xffffff;
-        // textfield.x = 172;
-        // textfield.y = 135;
-        // this.textfield = textfield;
-
         GameInstance.instance.main = this;
         GameInstance.instance.initLayer();      
         ProcessManager.clientReady();
@@ -171,46 +128,12 @@ class Main extends eui.UILayer {
         result.texture = texture;
         return result;
     }
-    // /**
-    //  * 描述文件加载成功，开始播放动画
-    //  * Description file loading is successful, start to play the animation
-    //  */
-    // private startAnimation(result: Array<any>): void {
-    //     let parser = new egret.HtmlTextParser();
-
-    //     let textflowArr = result.map(text => parser.parse(text));
-    //     let textfield = this.textfield;
-    //     let count = -1;
-    //     let change = () => {
-    //         count++;
-    //         if (count >= textflowArr.length) {
-    //             count = 0;
-    //         }
-    //         let textFlow = textflowArr[count];
-
-    //         // 切换描述内容
-    //         // Switch to described content
-    //         textfield.textFlow = textFlow;
-    //         let tw = egret.Tween.get(textfield);
-    //         tw.to({ "alpha": 1 }, 200);
-    //         tw.wait(2000);
-    //         tw.to({ "alpha": 0 }, 200);
-    //         tw.call(change, this);
-    //     };
-
-    //     change();
-    // }
 
     /**
      * 点击按钮
      * Click the button
      */
-    private onButtonClick(e: egret.TouchEvent) {
-        // let panel = new eui.Panel();
-        // panel.title = "Title";
-        // panel.horizontalCenter = 0;
-        // panel.verticalCenter = 0;
-        // this.addChild(panel);
+    private onButtonClick(e: egret.TouchEvent) {       
         PipeManager.sendMsg(PipeConstants.GET_TEST_PANEL);
     }
 }
