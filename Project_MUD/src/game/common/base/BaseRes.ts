f56fca106168e86e7cc7e5f079959709a55d1911
lcxm3300 <lcxm3300@outlook.com>
2021-01-05 21:03:43 +0800

    增加视野范围
    除了战斗范围外，增加视野范围

--- Project_MUD/src/game/common/base/BaseRes.ts
+++ Project_MUD/src/game/common/base/BaseRes.ts
@@ -8,6 +8,7 @@ class BaseAliveRes extends BaseRes{
 	public _level:number = 0;	
 	public _max_HP:number = 0;
 	public _max_MP:number = 0;
+	public _field_range:number = 1;
 	public _attack_range:number = 1;
 	public _cellSizeX:number = 100;
 	public _cellSizeY:number = 70;	
